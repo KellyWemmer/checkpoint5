50b6e448056334786876d348b29b788354d31efb
Kelly <kellysue3@yahoo.com>
2022-08-19 11:22:51 -0600

    initial commit

---
+++ src/models/ThreadPost.js
@@ -0,0 +1,12 @@
+export class ThreadPost {
+    constructor (data) {
+        this.id = data.id || ''
+        this.likeIds = data.likeIds 
+        this.imgUrl = data.imgUrl || ''
+        this.body = data.body || ''
+        this.creatorId = data.creatorId || ''
+        this.createdAt = data.createdAt || ''
+        this.updatedAt = data.updatedAt || ''
+        this.creator = data.creator || {}        
+    }
+}
